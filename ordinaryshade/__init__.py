72e743e7ce84148bb7595de367c8e2676de03ad5
Steven Shofner <shofner.steven@gmail.com>
2026-09-23 07:56:58 -0700

    Prepare OrdinaryShade 0.1.0a15 for Ordinary v0.1.10

--- ordinaryshade/__init__.py
+++ ordinaryshade/__init__.py
@@ -25,7 +25,7 @@ def unroll_range(*arguments):
     """Request compile-time expansion of a constant integer range."""
     return range(*arguments)
 
-__version__ = "0.1.0a14"
+__version__ = "0.1.0a15"
 
 __all__ = [
     "AccelerationStructure", "CompiledFunction", "CompiledShader", "CompilerUnavailableError", "ComputeShader", "ExternalFunction", "FixedArrayType", "GraphicsShader",
